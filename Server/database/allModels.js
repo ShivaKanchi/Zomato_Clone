8eb8c8bb66f255147b28b332a08239a600060fed
ShivaKanchi <shivakanchi111@gmail.com>
2022-11-18 16:17:52 +0530

    Started with project

---
+++ Server/database/allModels.js
@@ -0,0 +1,17 @@
+import { FoodModel } from './Food';
+import { UserModel } from './User';
+import { RestaurantModel } from './Restaurant';
+import { MenuModel } from './Menu';
+import { ReviewModel } from './Review';
+import { OrderModel } from './Order';
+import { ImageModel } from './Image';
+
+export {
+    UserModel,
+    RestaurantModel,
+    MenuModel,
+    FoodModel,
+    OrderModel,
+    ReviewModel,
+    ImageModel
+};
