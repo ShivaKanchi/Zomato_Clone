5ee0aef721461966ab186e2fbed307c864fc7930
ShivaKanchi <shivakanchi111@gmail.com>
2022-11-19 03:47:43 +0530

    Model creation done

--- Server/database/User/index.js
+++ Server/database/User/index.js
@@ -1,9 +1,20 @@
 import mongoose from "mongoose";
 
-const ImageSchema = new mongoose.Schema({
-
+const UserSchema = new mongoose.Schema({
+    fullname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String },
+    address: [{ detail: { type: String }, for: { type: String } }],
+    phone: [{ type: Number }]
 }, {
     timestamps: true
-})
+});
 
-export const ImageModel = mongoose.model("images", ImageSchema)
+//attachments of model
+UserSchema.methods.generateJwtToken = function () { };
+//helper functions
+//to check if this data exists or not
+UserSchema.statics.findByEmailAndPhone = async () => { };
+UserSchema.statics.findByEmailAndPassword = async () => { };
+
+export const UserModel = mongoose.model("users", UserSchema)
