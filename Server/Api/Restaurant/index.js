8e7ea9418321b6013c955309fba5fa9f361bcd24
ShivaKanchi <shivakanchi111@gmail.com>
2022-11-21 22:57:35 +0530

    Added routes for Rreview and order delete and select

--- Server/Api/Restaurant/index.js
+++ Server/Api/Restaurant/index.js
@@ -4,26 +4,26 @@ import { RestaurantModel } from "../../database/allModels";
 
 const Router = express.Router();
 
-/*
-*Route    /create
-*Desc     create a Order
-*Params   none
-*Method   POST
-*Access   Public
-*/
-Router.post("/create", async (req, res) => {
-    try {
-        const { data } = req.body;
-        const newData = await RestaurantModel.create({
-            ...data
-        });
-        return res.status(201).json({
-            success: true,
-            Order: newData
-        }); } catch (error) {
-        return res.status(500).json({ error: error.message });
-    }
-})
+// /*
+// *Route    /create
+// *Desc     create a Order
+// *Params   none
+// *Method   POST
+// *Access   Public
+// */
+// Router.post("/create", async (req, res) => {
+//     try {
+//         const { data } = req.body;
+//         const newData = await RestaurantModel.create({
+//             ...data
+//         });
+//         return res.status(201).json({
+//             success: true,
+//             Order: newData
+//         }); } catch (error) {
+//         return res.status(500).json({ error: error.message });
+//     }
+// })
 
 
 /*
--- Server/Api/Review/index.js
+++ Server/Api/Review/index.js
@@ -4,24 +4,24 @@ import { ReviewModel } from "../../database/allModels";
 
 const Router = express.Router();
 
-/*
-*Route    /create
-*Desc     create a Review
-*Params   none
-*Method   GET
-*Access   Public
-*/
-Router.get("/:resid", async (req, res) => {
-    try {
-        const { resid } = req.params;
-        const reviews = await ReviewModel.find({ restaurants: resid }).sort({
-            createdAt: -1,
-        });
-        return res.json({ reviews });
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
-    }
-})
+// /*
+// *Route    /create
+// *Desc     create a Review
+// *Params   none
+// *Method   GET
+// *Access   Public
+// */
+// Router.get("/:resid", async (req, res) => {
+//     try {
+//         const { resid } = req.params;
+//         const reviews = await ReviewModel.find({ restaurants: resid }).sort({
+//             createdAt: -1,
+//         });
+//         return res.json({ reviews });
+//     } catch (error) {
+//         return res.status(500).json({ error: error.message });
+//     }
+// })
 
 
 /*
