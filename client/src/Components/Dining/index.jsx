c33d88bd051369cc4213bff5bfba58737c1ada78
ShivaKanchi <shivakanchi111@gmail.com>
2022-11-23 20:38:44 +0530

    Completed restaurant card and dining page

--- client/src/Components/Dining/index.jsx
+++ client/src/Components/Dining/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import DiningCarousel from './DiningCarousel'
 const Dining = () => {
     return (
-        <div>Dining</div>
+        <div className="mb-10">
+            <h1 className="text-xl mt-4 md:mt-8 md:text-3xl md:font-semibold">
+                Collections
+            </h1>
+            <p className='text-sm font-light mb-5 md:text-xl'>
+                Explore curated lists of top restaurants, cafes, pubs, and bars in Mumbai, based on trends
+            </p>
+            <DiningCarousel />
+        </div>
     )
 }
 
