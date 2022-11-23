1d5d8042c3fa3c08e59c2f98a93ddc8317f65f80
ShivaKanchi <shivakanchi111@gmail.com>
2022-11-23 17:36:37 +0530

    Homepage tabs

--- client/src/Pages/Home.page.jsx
+++ client/src/Pages/Home.page.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useParams } from "react-router-dom";
+
+//components
+import Delivery from '../Components/Delivery';
+import Dining from '../Components/Dining';
+import NightLife from '../Components/NightLife';
+import Nutrition from '../Components/Nutrition';
 import HomeLayout from '../Layouts/Homepage.layout'
 
 const Home = () => {
+    const { type } = useParams();
     return (
-        <div>Home</div>
+        <>
+            <div className="my-5 mb-20 md:mb-10">
+                {type === "delivery" && <Delivery />}
+                {type === "dining" && <Dining />}
+                {type === "night" && <NightLife />}
+                {type === "nutrition" && <Nutrition />}
+            </div>
+        </>
     )
 }
 
