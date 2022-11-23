821c2cb95ea74fea53a26076b728dd48f4f0bcaa
ShivaKanchi <shivakanchi111@gmail.com>
2022-11-24 03:22:00 +0530

    Restaurant page created

--- client/src/Pages/Restaurant.page.jsx
+++ client/src/Pages/Restaurant.page.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useParams, useLocation, Navigate } from "react-router-dom";
 import { Outlet } from 'react-router-dom'
 
 const Restaurant = () => {
-    return (<>
-        <h2>Restaurant Page</h2>
-        <Outlet />
-    </>
+    const { id } = useParams();
+    const { pathname } = useLocation();
+
+    if (`/restaurant/${id}` === pathname) {
+        return <Navigate to={`/restaurant/${id}/overview`} />;
+    }
+
+    return (
+        <>
+            <Outlet />
+        </>
     )
 }
 
