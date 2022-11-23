017931e6e5c1c6f4190466d123a0e1043a5ff920
ShivaKanchi <shivakanchi111@gmail.com>
2022-11-23 17:42:06 +0530

    Delivery page started

--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+const DeliverySmCard = ({ image, title }) => { };
+const DeliveryLgCard = ({ image, title }) => { };
+
+
 const Delivery = () => {
     return (
         <div>Delivery</div>
