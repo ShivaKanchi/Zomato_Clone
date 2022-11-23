e4322ad7c8797c01faf678ed9f29796904693109
ShivaKanchi <shivakanchi111@gmail.com>
2022-11-23 20:07:53 +0530

    Swiper done

--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -1,10 +1,55 @@
 import React, { useState } from 'react';
+import RestaurantCard from '../RestaurantCard';
 
 //components
 import DeliveryCarousel from './DeliveryCarousel';
 // import 
 const Delivery = () => {
-    const [restaurantlist, setRestaurantlist] = useState([]);
+    const [restaurantlist, setRestaurantlist] = useState([
+        {
+            _id: "124ksjf435245jv34fg3",
+            isPro: true,
+            isOff: true,
+            name: "Nathu's Sweets",
+            restaurantReviewValue: "3.7",
+            cuisine: [
+                "Mithai",
+                "South Indian",
+                "Chinese",
+                "Street Food",
+                "Fast Food",
+                "Desserts",
+                "North Indian",
+            ],
+            averageCost: "450",
+        },
+        {
+            _id: "sdffdsadadsfadfadsfadsf",
+            isPro: true,
+            isOff: false,
+            name: "Master Koii's",
+            restaurantReviewValue: "4.6",
+            cuisine: ["Asian", "Chinese", "Thai", "Malaysian", "Korean"],
+            averageCost: "600",
+        },
+        {
+            _id: "124ksjf435245jfdfv34fg3",
+            isPro: true,
+            isOff: true,
+            name: "Nathu's Sweets",
+            restaurantReviewValue: "3.7",
+            cuisine: [
+                "Mithai",
+                "South Indian",
+                "Chinese",
+                "Street Food",
+                "Fast Food",
+                "Desserts",
+                "North Indian",
+            ],
+            averageCost: "450",
+        },
+    ]);
     return (
         <>
 
@@ -14,7 +59,7 @@ const Delivery = () => {
             </h1>
             <div className='flex justify-between flex-wrap mt-5'>
                 {restaurantlist.map((restaurant) => (
-                    <>  </> // <RestaurantCard {...restaurant} key={restaurant._id} />
+                    <RestaurantCard {...restaurant} key={restaurant._id} />
                 ))}
             </div>
         </>
