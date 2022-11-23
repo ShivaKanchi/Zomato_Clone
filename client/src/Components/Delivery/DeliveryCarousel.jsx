e33a173864108f3a219630f440b4d4816dc92306
ShivaKanchi <shivakanchi111@gmail.com>
2022-11-23 19:35:57 +0530

    Swiper done

--- client/src/Components/Delivery/DeliveryCarousel.jsx
+++ client/src/Components/Delivery/DeliveryCarousel.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import "swiper/css"
+import "swiper/css/navigation";
 import { Navigation } from "swiper";
 import DeliveryCategoryCard from './DeliveryCategoryCard'
 
